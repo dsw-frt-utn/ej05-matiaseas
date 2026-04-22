0cc22fa880e6fb65a6ffb38f4b3ca4bf7c72741a
matiaseas <matias.eas.unt@gmail.com>
2026-04-22 15:45:53 -0300

    Arreglada lógica de calculo de consumo

--- Dsw2026Ej5/Domain/VehiculoCombustible.cs
+++ Dsw2026Ej5/Domain/VehiculoCombustible.cs
@@ -4,30 +4,29 @@ using System.Text;
 
 namespace Dsw2026Ej5.Domain;
 
-public class VehiculoCombustible: Vehiculo
+public class VehiculoCombustible : Vehiculo
 {
     private double kilometrosPorLitro;
     private double litrosExtra;
 
-    public VehiculoCombustible(string patente, string marca, string modelo, int anio, double capacidadCarga, 
-        Sucursal sucursal, double kilometrosPorLitro, double litrosExtra) : base(VehiculoTipo.Combustible, patente, marca, modelo, anio, capacidadCarga, sucursal)
+    public VehiculoCombustible(string patente, string marca, string modelo, int anio, double capacidadCarga,
+                               Sucursal sucursal, double kilometrosPorLitro, double litrosExtra)
+        : base(VehiculoTipo.Combustible, patente, marca, modelo, anio, capacidadCarga, sucursal)
     {
         this.kilometrosPorLitro = kilometrosPorLitro;
         this.litrosExtra = litrosExtra;
     }
 
-    public double GetKilometrosPorLitro()
-    {
-        return kilometrosPorLitro;
-    }
-
-    public double GetLitrosExtra()
-    {
-        return litrosExtra;
-    }
+    public double GetKilometrosPorLitro() => kilometrosPorLitro;
+    public double GetLitrosExtra() => litrosExtra;
 
     public override double CalcularConsumo(double kilometros)
     {
-        return kilometros * kilometrosPorLitro;
+        double total = kilometros / kilometrosPorLitro;
+        if ((2026 - GetAnio()) > 5)
+        {
+            total = kilometros / kilometrosPorLitro + litrosExtra * (kilometros / 15);
+        }
+        return total;
     }
 }
